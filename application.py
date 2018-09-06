29b7634ce3aaafb9f2714feb3e7511898d39ff6e
dave nune <david.q.nunez@gmail.com>
2018-09-05 23:50:49 -0400

    added functionality to delete posts

--- application.py
+++ application.py
@@ -2,22 +2,51 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app)
 
 # Creates a list of messages
 message_list = []
 
+# Create a hashmap of channels. ugc = user generated channels
+ugc = {"General"}
+
 # Route renders index page
 @app.route("/")
 def hello():
-    return render_template("index.html", user_posts = message_list )
+    return render_template("index.html", user_posts=message_list)
+    # return render_template("index.html", user_posts = ugc )
 
-#Route for adding posts
+
+# Route for adding posts
 @socketio.on("create a new post")
 def user_post(data):
-    print("grab post data: ", data)
+    # print("grab post data: ", data)
+    # emit("add post to app", data, broadcast=True)
+    print("inside post", data)
+    user_post = {
+        "username": data["username"],
+        "post": data["post"],
+        "hash": data["hash"],
+        "timestamp": data["timestamp"],
+    }
+    message_list.append(user_post)
     emit("add post to app", data, broadcast=True)
+
+
+# Route for deleting posts
+@socketio.on("delete post")
+def post(data):
+    print("inside delete post func", data)
+    print("mess list", message_list)
+    for post in message_list:
+        print("inside arr", post)
+        if post["hash"] == data["hash"]:
+            print("i am inside posttt deleteee", data)
+            message_list.remove(post)
+            emit("remove post", data, broadcast=True)
+            break
+
 
 # Create votes
 @socketio.on("submit vote")
--- static/index.js
+++ static/index.js
@@ -1,3 +1,20 @@
+// Establish connection with socket based on my current URL PATH and PORT
+let socket = io.connect("http://" + document.domain + ":" + location.port);
+
+// Create unique hash for each post based on username and timestamp
+let post_hash = data => {
+  return btoa(data.post + data.username + data.timestamp);
+};
+
+// Function to add delete button
+let delete_icon = post => {
+  let delete_icon_html = '<i onClick="delete_post(this)" ';
+  delete_icon_html += `data-hash="${post.getAttribute(
+    "data-hash"
+  )}" name="close-circle" class="fa fa-close close"> </i>`;
+  return delete_icon_html;
+};
+
 // Get current timestamp
 let create_timestamp = () => {
   let date = new Date();
@@ -8,9 +25,19 @@ let create_timestamp = () => {
   return `${hours}:${minutes}:${seconds}`;
 };
 
+// helper function to delete posts
+let delete_post = post => {
+  console.log("you hit delete post func");
+  let data = {
+    hash: post.getAttribute("data-hash")
+  };
+  socket.emit("delete post", data);
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   // Establish connection with socket based on my current URL PATH and PORT
-  let socket = io.connect("http://" + document.domain + ":" + location.port);
+  // let socket = io.connect("http://" + document.domain + ":" + location.port);
+
   socket.on("connect", function() {
     // Don't allow user to create an empty display name
     document.querySelector("#displayname").onkeyup = () => {
@@ -62,7 +89,20 @@ document.addEventListener("DOMContentLoaded", () => {
         username: localStorage.getItem("username"),
         timestamp: timestamp
       };
+      // alloc unique hash to identify div
+      data.hash = post_hash(data);
+
       socket.emit("create a new post", data);
+    });
+
+    //Puts the delete icon on each post
+    document.querySelectorAll(".post").forEach(post => {
+      if (
+        post.getAttribute("data-username") == localStorage.getItem("username")
+      ) {
+        post.classList.add("current");
+        post.innerHTML += delete_icon(post);
+      }
     });
   });
 
@@ -70,10 +110,40 @@ document.addEventListener("DOMContentLoaded", () => {
   socket.on("add post to app", post => {
     console.log("post meta data emitted: ", post);
     let div = document.createElement("div");
-    div.innerText = `${post.username}: ${post.post}`;
+
+    div.setAttribute("data-hash", post.hash);
+    div.setAttribute("data-username", post.username);
+
+    // used so we can target and delete posts
+    div.classList.add("post");
+
+    div.innerHTML =
+      post.post + ' <span class="username">' + post.username + "</spam>";
+    div.innerHTML += ' <span class="timestamp">' + post.timestamp + "</spam>";
+
+    if (post.username == localStorage.getItem("username")) {
+      // Hash allows us to delete posts
+      div.classList.add("current");
+      div.innerHTML += `<i data-hash="${
+        post.hash
+      }" class="fa fa-close close"></i>`;
+    }
 
     // add message to page
     document.querySelector(".message_posts").append(div);
+
+    // add ability to delete message
+    document.querySelectorAll(".close").forEach(post => {
+      post.addEventListener("click", () => {
+        delete_post(post);
+      });
+    });
+  });
+
+  // removes posts based on hash
+  socket.on("remove post", post => {
+    console.log("going to remove: ", post);
+    document.querySelector('.post[data-hash="' + post.hash + '"]').remove();
   });
 
   // When a new vote is announced, add to the unordered list
